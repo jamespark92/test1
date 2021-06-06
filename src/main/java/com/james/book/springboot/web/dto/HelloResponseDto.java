7bf37db1a35a0b1965dfeed3f53064e74836ffd4
junyoung <pjyoun915@naver.com>
2021-06-06 20:09:44 +0900

    210606_helloController

---
+++ src/main/java/com/james/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.james.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter   //1
+@RequiredArgsConstructor // 2
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
